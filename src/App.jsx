a6d658c2655ab082e1dbf77b529ff46365f6e5b4
Rishi Singh <rs3949427@gmail.com>
2023-10-24 22:37:47 +0530

    Added Routing (Login/SignUp)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
-import './App.css';
-import Home from './pages/Home';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Home></Home>
+    ),
+  },
+  {
+    path: "/login",
+    element: <LoginPage></LoginPage>,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage></SignupPage>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Login from '../features/auth/components/Login';
+const LoginPage = () => {
+  return (
+    <Login/>
+  )
+}
+
+export default LoginPage
